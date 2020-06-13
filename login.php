3ce0173fc572de9fb4bfcffeb17d6c01bced6b42
Tyler Edwards <tedwards@halosight.com>
2020-06-13 01:30:07 -0600

    fix

--- login.php
+++ login.php
@@ -111,7 +111,7 @@ class setSessionVars{
         $_SESSION["firstname"] = $this->firstname;
         $_SESSION["lastname"] = $this->lastname;
         $_SESSION["email"] = $this->email;
-        error_log("This is the username: " . $this->username);
+        error_log("This is the username: " . $_SESSION["username"]);
 
         // Set cookies
         $hour=time()+3600*24*36000;
