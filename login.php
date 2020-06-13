5763218212d05f9285622f091f83d871b5c57ac5
Tyler Edwards <tedwards@halosight.com>
2020-06-13 01:24:22 -0600

    fix

--- login.php
+++ login.php
@@ -63,6 +63,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                             session_start();
 
                             $setSession = new setSessionVars($id, $username, $firstname, $lastname, $email);
+                            $setSession->setSessionVariables();
 
                         } else {
                             // Display an error message if password is not valid
