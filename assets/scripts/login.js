6bf0e2e61276519879c49385b3747fe002ee303c
Tyler Edwards <tedwards@halosight.com>
2020-06-15 01:28:00 -0600

    login

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,7 +1,7 @@
 $(".js-sign-in").on('click', function(){
-    window.location.href('/bucketlist/login.php');
+    window.location.href = '/bucketlist/login.php';
 });
 
 $(".js-register").on('click', function(){
-    window.location.href('/bucketlist/register.php');
+    window.location.href = '/bucketlist/register.php';
 });
