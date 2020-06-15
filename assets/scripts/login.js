43bbd8c3692fdc4a14ed4df080aedef3e6b89b5d
Tyler Edwards <tedwards@halosight.com>
2020-06-15 01:02:01 -0600

    login

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -4,3 +4,10 @@ $(".js-sign-in").on('click', function(){
     $('.js-login-form').addClass('d-block').removeClass('d-none');
     $('.js-register-form').addClass('d-none').removeClass('d-block');
 });
+
+$(".js-register").on('click', function(){
+    $(this).addClass('active').removeClass('inactive');
+    $('.js-sign-in').addClass('inactive').removeClass('active');
+    $('.js-register-form').addClass('d-block').removeClass('d-none');
+    $('.js-login-form').addClass('d-none').removeClass('d-block');
+});
