d9cc5652e72785c2c9724e439dd3dc9b753750a5
Tyler Edwards <tedwards@halosight.com>
2020-06-15 02:17:18 -0600

    test

--- actions/send_email.php
+++ actions/send_email.php
@@ -5,9 +5,39 @@ class SendEmail{
     private $subject;
     private $message;
     private $from;
+    private $header;
+    private $firstname;
 
-    function EmailTemplate(){
-        return '';
+    public function __construct($to, $subject, $from, $header, $firstname = "", $message = 1){
+        $this->to = $to;
+        $this->subject = $subject;
+        $this->message = $message;
+        $this->from = $from;
+        $this->header = $header;
+        $this->firstname = $firstname;
+    }
+
+    function SendEmailFromTemplate(){
+        $txt = "<html>
+                    <head>
+                    <title>Gobinit Password Reset</title>
+                    </head>
+                    <body>
+                    <p>Hello, " . $this->firstname . ", this is a test email.</p>
+                    <table>
+                    <tr>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    </tr>
+                    <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    </tr>
+                    </table>
+                    </body>
+                </html>";
+
+        mail($this->to,$this->subject,$txt,$this->headers);
     }
 }
 
--- forgot-password.php
+++ forgot-password.php
@@ -1,5 +1,54 @@
 <?php
+require_once "/home1/gobinitc/public_html/bucketlist/config.php";
+require_once "/home1/gobinitc/public_html/bucketlist/send_email.php";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $to_email = "";
+    $status = "";
+    $firstname = "";
+    $valid_config = false;
+    if($_POST["email"] == "" || !isset($_POST["email"])){
+        $status = "Please enter an email";
+    } else {
+        $valid_config = true;
+    }
 
+    if($valid_config){
+        $sql = "SELECT email, firstname FROM users WHERE email = ? OR username = ?";
+        if($stmt = $mysqli->prepare($sql)){
+            $stmt->bind_param("ss", $param_email, $param_username);
+            $param_email = $_POST["email"];
+            $param_username = $_POST["email"];
+            if($stmt->execute()){
+                $stmt->store_result();
+
+                if($stmt->num_rows == 1){
+                    $stmt->bind_result($email, $first_name);
+                    while($stmt->fetch()){
+                        $to_email = $email;
+                        $firstname = $first_name;
+                    }
+                } else {
+                    $status = "An error occurred fetching your account";
+                    $valid_config = false;
+                }
+            } else {
+                $status = "An error occurred fetching your account";
+                $valid_config = false;
+                error_log("An error occurred fetching the account for " . $param_email);
+            }
+        } else {
+            $status = "An error occurred fetching your account";
+            $valid_config = false;
+        }
+    }
+
+    if($valid_config && $to_email != ""){
+        $headers = "From: Gobinit Support <support@gobinit.com>\r\n" . "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $subject = "Gobinit Password Reset";
+        $from = "support@gobinit.com";
+        $email = new SendEmail($email, $subject, $from, $firstname);
+    }
+}
 ?>
 
 <!DOCTYPE html>
