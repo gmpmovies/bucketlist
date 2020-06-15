e25fabb77ebbb85b97e5852003c93c0e7a18d37d
Tyler Edwards <tedwards@halosight.com>
2020-06-14 19:35:19 -0600

    login

--- html_style_head.php
+++ html_style_head.php
@@ -4,6 +4,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
-<!--<link rel="stylesheet" href="/bucketlist/assets/styles/default.css">-->
+<link rel="stylesheet" href="/bucketlist/assets/styles/default.css">
 <?php require_once "/home1/gobinitc/public_html/bucketlist/global/favicon.php"?>
 
