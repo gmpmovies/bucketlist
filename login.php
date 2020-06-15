5ee24369386086f9e8ac2206e8ac9ca45a53876e
Tyler Edwards <tedwards@halosight.com>
2020-06-14 19:25:54 -0600

    login

--- login.php
+++ login.php
@@ -12,32 +12,35 @@ require_once "/home1/gobinitc/public_html/bucketlist/actions/login.php";
 <body>
 <?php require_once "/home1/gobinitc/public_html/bucketlist/navbar.php"; ?>
 <div class="default-margin">
-    <div class="wrapper fadeInDown">
-        <div id="formContent">
-            <!-- Tabs Titles -->
-            <h2 class="active"> Sign In </h2>
-            <h2 class="inactive underlineHover">Sign Up </h2>
+    <div class="" style="margin: auto;">
+        <div class="wrapper fadeInDown">
+            <div id="formContent">
+                <!-- Tabs Titles -->
+                <h2 class="active"> Sign In </h2>
+                <h2 class="inactive underlineHover">Sign Up </h2>
 
-            <!-- Icon -->
-            <div class="fadeIn first">
-                <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+                <!-- Icon -->
+                <div class="fadeIn first">
+                    <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+                </div>
+
+                <!-- Login Form -->
+                <form>
+                    <input type="text" id="login" class="fadeIn second" name="username" placeholder="login">
+                    <span class="help-block"><?php echo $username_err; ?></span>
+                    <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
+                    <span class="help-block"><?php echo $password_err; ?></span>
+                    <input type="submit" class="fadeIn fourth" value="Log In">
+                </form>
+
+                <!-- Remind Passowrd -->
+                <div id="formFooter">
+                    <a class="underlineHover" href="#">Forgot Password?</a>
+                </div>
+
             </div>
-
-            <!-- Login Form -->
-            <form>
-                <input type="text" id="login" class="fadeIn second" name="username" placeholder="login">
-                <span class="help-block"><?php echo $username_err; ?></span>
-                <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
-                <span class="help-block"><?php echo $password_err; ?></span>
-                <input type="submit" class="fadeIn fourth" value="Log In">
-            </form>
-
-            <!-- Remind Passowrd -->
-            <div id="formFooter">
-                <a class="underlineHover" href="#">Forgot Password?</a>
-            </div>
-
         </div>
+
     </div>
 </div>
 
